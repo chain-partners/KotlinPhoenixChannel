9d567e3d5df7ad46332a4409c82d3a463e5e9746
changhee@chain.partners <changhee@chain.partners>
2018-04-19 11:14:18 +0900

    Add methods to SocketListener

--- src/main/kotlin/PhoenixSocketListener.kt
+++ src/main/kotlin/PhoenixSocketListener.kt
@@ -1,1 +1,17 @@
-interface PhoenixSocketListener
+import okhttp3.Response
+
+/**
+ * Each callbacks corresponds to that of [okhttp3.WebSocketListener].
+ */
+interface PhoenixSocketListener {
+
+  fun onOpen(response: Response?)
+
+  fun onClosing(code: Int?, reason: String?)
+
+  fun onClosed(code: Int?, reason: String?)
+
+  fun onFailure(t: Throwable?, response: Response?)
+
+  fun onMessage(text: String?)
+}
